78e938e6f3f533278a0fd54b807a7e663974ef8d
Aneta <adam.pierzchala.muzyk@gmail.com>
2020-10-26 22:07:46 +0100

    start

---
+++ src/test/java/pl/pierzchala/ip/IpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pierzchala.ip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
